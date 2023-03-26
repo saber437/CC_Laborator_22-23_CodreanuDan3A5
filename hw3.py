391f52876d9bf0d625e61433350256a8200d990b
Codreanu Dan <50355298+saber437@users.noreply.github.com>
2023-03-26 23:16:34 +0300

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -8,3 +8,13 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'path/to/your/credentials.json'
 
 # Connect to Firestore
 db = firestore.Client()
+
+# cloud Storage
+storage_client = storage.Client()
+bucket_name = 'bucket_name'
+bucket = storage_client.get_bucket(bucket_name)
+
+# cloud pub/sub
+publisher = pubsub_v1.PublisherClient()
+topic_name = 'topic_name'
+topic_path = publisher.topic_path('project_id', topic_name)
